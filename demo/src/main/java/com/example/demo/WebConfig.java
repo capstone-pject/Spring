5971a5bc8805d2e3c624716b6bce6c88a50c2553
jihoon629 <jihonn629@gmail.com>
2025-06-05 03:21:01 +0900

    cros 변경

--- demo/src/main/java/com/example/demo/WebConfig.java
+++ demo/src/main/java/com/example/demo/WebConfig.java
@@ -8,7 +8,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // API 경로 패턴
+        registry.addMapping("/*/**") // API 경로 패턴
                 .allowedOrigins("http://localhost:3000") // React 개발 서버 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
